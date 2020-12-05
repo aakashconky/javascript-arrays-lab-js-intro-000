582d1ad670f9e8a223bfcf8bf1be4308544f3266
aakash thapa <aakash_conky@hotmail.com>
2020-12-05 23:42:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
