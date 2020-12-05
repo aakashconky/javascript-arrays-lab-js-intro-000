666fa4e080df1d2c2953ebb94e1de19583ed6f80
aakash thapa <aakash_conky@hotmail.com>
2020-12-05 23:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKiten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKiten() {
+  kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name,...kittens]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+fucntion removeLastKitten() {
+  return kittens.slice(0,kittens.length- 1)
+}
